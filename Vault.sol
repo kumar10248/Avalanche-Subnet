aae9ce6198a48e6b6d849e9ed021abba6404aba5
kumar Devashish <kumardevashish000@gmail.com>
2024-07-11 23:27:42 +0530

    chore: Update ERC20 contract formatting and event declaration

--- Vault.sol
+++ Vault.sol
@@ -8,7 +8,10 @@ interface IERC20 {
 
     function transfer(address recipient, uint amount) external returns (bool);
 
-    function allowance(address owner, address spender) external view returns (uint);
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint);
 
     function approve(address spender, uint amount) external returns (bool);
 
