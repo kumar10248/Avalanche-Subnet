cc5f56b4b58f761772eb5df696b2f271bf248a4e
kumar Devashish <kumardevashish000@gmail.com>
2024-07-13 23:42:00 +0530

    chore: Refactor Vault contract to track depositors and distribute rewards
    This commit refactors the Vault contract to track depositors and distribute rewards. It introduces two new variables, `depositors` and `isDepositor`, to keep track of the addresses that have made deposits. The `_mint` and `_burn` functions are updated to add and remove depositors from the `depositors` array. Additionally, a new function `distributeRewards` is added to distribute rewards to depositors based on their share of the total balance. This enhancement allows for more efficient reward distribution and better tracking of depositors.

--- Vault.sol
+++ Vault.sol
@@ -3,24 +3,14 @@ pragma solidity ^0.8.17;
 
 interface IERC20 {
     function totalSupply() external view returns (uint);
-
     function balanceOf(address account) external view returns (uint);
-
     function transfer(address recipient, uint amount) external returns (bool);
-
-    function allowance(
-        address owner,
-        address spender
-    ) external view returns (uint);
-
+    function allowance(address owner, address spender) external view returns (uint);
     function approve(address spender, uint amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
-
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+    function mint(uint amount) external;
+    function burn(uint amount) external;
+    function pendingReward(address account) external view returns (uint);
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 }
@@ -30,12 +20,18 @@ contract Vault {
 
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
+    address[] public depositors; // Array to track depositors
+    mapping(address => bool) public isDepositor; // To avoid duplicate entries in depositors array
 
     constructor(address _token) {
         token = IERC20(_token);
     }
 
     function _mint(address _to, uint _shares) private {
+        if (!isDepositor[_to]) {
+            isDepositor[_to] = true;
+            depositors.push(_to);
+        }
         totalSupply += _shares;
         balanceOf[_to] += _shares;
     }
@@ -43,19 +39,12 @@ contract Vault {
     function _burn(address _from, uint _shares) private {
         totalSupply -= _shares;
         balanceOf[_from] -= _shares;
+        if (balanceOf[_from] == 0) {
+            isDepositor[_from] = false;
+        }
     }
 
     function deposit(uint _amount) external {
-        /*
-        a = amount
-        B = balance of token before deposit
-        T = total supply
-        s = shares to mint
-
-        (T + s) / T = (a + B) / B 
-
-        s = aT / B
-        */
         uint shares;
         if (totalSupply == 0) {
             shares = _amount;
@@ -68,18 +57,23 @@ contract Vault {
     }
 
     function withdraw(uint _shares) external {
-        /*
-        a = amount
-        B = balance of token before withdraw
-        T = total supply
-        s = shares to burn
-
-        (T - s) / T = (B - a) / B 
-
-        a = sB / T
-        */
         uint amount = (_shares * token.balanceOf(address(this))) / totalSupply;
         _burn(msg.sender, _shares);
         token.transfer(msg.sender, amount);
+    }
+
+    function distributeRewards() external {
+        uint reward = token.pendingReward(address(this));
+        if (reward > 0) {
+            token.mint(reward);
+            uint totalBalance = token.balanceOf(address(this));
+            for (uint i = 0; i < depositors.length; i++) {
+                address account = depositors[i];
+                if (balanceOf[account] > 0) {
+                    uint share = (balanceOf[account] * reward) / totalBalance;
+                    token.transfer(account, share);
+                }
+            }
+        }
     }
 }
